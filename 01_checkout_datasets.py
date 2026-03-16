97def8e7b632b2a0a5ba48989aded6b7c2d37b2f
Sabrina Otto <pinkfloat@tuta.com>
2026-03-16 13:02:22 +0100

    cleanup: translate prints of 01_checkout_datasets.py

--- 01_checkout_datasets.py
+++ 01_checkout_datasets.py
@@ -4,28 +4,28 @@ import geopandas as gpd
 # Set display option to show all columns
 pd.set_option('display.max_columns', None)
 
-print("Trinkbrunnen (Berlin Open Data / BWB):")
+print("Drinking fountains (Berlin Open Data / BWB):")
 trinkbrunnen_gdf = gpd.read_file("Trinkbrunnen/trinkwasserbrunnen_trinkwasserbrunnen_WGS84.geojson")
-print(f"Num Trinkbrunnen: {len(trinkbrunnen_gdf)}")
+print(f"Number of drinking fountains: {len(trinkbrunnen_gdf)}")
 print(trinkbrunnen_gdf.head())
 
 
-print("\n\nFlächen insgesamt:")
-print("\n\nMerged Data Set von Berlin Open Data:")
+print("\n\nLand use areas:")
+print("Merged dataset from Berlin Open Data:")
 berlin_area_df = gpd.read_file("Flaechennutzung/berlin_area_merged.geojson")
-print(f"Num Flächen im Dataset: {len(berlin_area_df)}")
+print(f"Number of areas in the dataset: {len(berlin_area_df)}")
 print(berlin_area_df.head())
 
 
-print("\n\nHaltestellen:")
+print("\n\nPublic transport stops:")
 haltestellen_osmnx_df = pd.read_csv("Haltestellen/oepnv_02_osmnx_result.csv")
-print(f"Num Haltestellen in OSM Dataset (osmnx): {len(haltestellen_osmnx_df)}")
-print(f"Num von Eintraegen mit Namen: {haltestellen_osmnx_df["name"].notna().sum()}")
+print(f"Number of stops in the OSM dataset (osmnx): {len(haltestellen_osmnx_df)}")
+print(f"Number of entries with names: {haltestellen_osmnx_df['name'].notna().sum()}")
 print(haltestellen_osmnx_df.head())
 
 
-print("\n\nGetränkeläden:")
+print("\n\nBeverage shops:")
 stores_osmnx_df = pd.read_csv("Getraenke_Laeden/stores_02_osmnx_result.csv")
-print(f"Num Stores in OSM Dataset (osmnx): {len(stores_osmnx_df)}")
-print(f"Num von Eintraegen mit Namen: {stores_osmnx_df["name"].notna().sum()}")
+print(f"Number of stores in the OSM dataset (osmnx): {len(stores_osmnx_df)}")
+print(f"Number of entries with names: {stores_osmnx_df['name'].notna().sum()}")
 print(stores_osmnx_df.head())
